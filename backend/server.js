205844443ef5018fad1ced0a52b215fe849db445
kolesovdanil35-droid <kolesovdanil35@gmail.com>
2026-06-20 17:50:05 +0500

    logs

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const app = require('./src/app');
 const migrate = require('./src/db/migrate');
 const port = process.env.PORT || 3001;
-
+console.log('DB_HOST=', process.env.DB_HOST);
+console.log('POSTGRES_HOST=', process.env.POSTGRES_HOST);
 migrate().then(() => {
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
